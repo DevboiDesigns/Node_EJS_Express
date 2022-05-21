cbfb59b4fb39988f62e1b0307c0e37dcaf90e46a
Christopher Hicks <visrule@protonmail.com>
2022-05-20 23:24:03 -0500

    add 404 error for not found

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,18 +1,23 @@
 // Third party modules
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 // Local files
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 // Parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Order still matters 
-app.use(adminRoutes)
-app.use(shopRoutes)
+// Order still matters
+app.use(adminRoutes);
+app.use(shopRoutes);
 
+// Error Handling with catch all
+app.use((req, res, next) => {
+  // set status code to 404 = page not found
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
